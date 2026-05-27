091c7193a8456a58ae978cc84b547ba607b3f916
Hemabollimuntha <118984114+Hemabollimuntha@users.noreply.github.com>
2026-05-27 15:15:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-const form =
-document.getElementById("contactForm");
 
-const successMessage =
-document.getElementById("successMessage");
-
-form.addEventListener("submit", function(event){
-
-    event.preventDefault();
-
-    successMessage.style.display = "block";
-
-    form.reset();
-
-}); 
